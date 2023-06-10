6424c019f018a0c419cc09bc74570ee832ef789c
Ajilore Daniel <ajiloredaniel58@gmail.com>
2023-06-10 02:47:12 -0700

    uy

--- src/pages/Home/ExclusiveSection/Exclusive.jsx
+++ src/pages/Home/ExclusiveSection/Exclusive.jsx
@@ -36,8 +36,8 @@ const Exclusive = () => {
       {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToShow: 4,
+          slidesToScroll: 4,
           infinite: true,
         }
       },
